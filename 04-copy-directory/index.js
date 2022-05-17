e5018efe2b315cb36e1f4d8f75941edbda5336af
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-05-17 16:04:31 +0300

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const path = require('path');
+const fs = require('fs');
 
+function copyDir(dir) {
+  fs.mkdir(
+    path.join(__dirname, `${dir}-copy`),
+    { recursive: true },
+    error => {
+      if (error) console.error(error.message);
+      copyFiles(dir);
+    }
+  );
+}
+
+function copyFiles(dir) {
+  fs.readdir(
+    path.join(__dirname, dir),
+    {withFileTypes: true},
+    (error, files) => {
+      if (error) console.error(error.message);
+
+      files.forEach(file => {
+        if (file.isDirectory()) return;
+        fs.copyFile(
+          path.join(__dirname, dir, file.name),
+          path.join(__dirname, `${dir}-copy`, file.name),
+          error => {
+            if (error) console.error(error.message);
+          }
+        );
+      });
+    }
+  );
+}
+
+copyDir('files');
